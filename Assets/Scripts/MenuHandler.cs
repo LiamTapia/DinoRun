c154a7e0bc0674c24177b0a26a8bfdfa1caaa283
LiamTapia <36176046+LiamTapia@users.noreply.github.com>
2021-05-08 22:22:58 -0700

    First commit

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject[] Menus;
+
+    public void changeMenu(int option)
+    {
+        moveMenuOut();
+        Menus[option].transform.localPosition = new Vector3(-350,0,0);
+        /*switch(option)
+        {
+            //Pone menu principa;
+            case 0:
+                moveMenuOut();
+                Menus[]
+                break;
+        }*/
+    }
+
+    public void moveMenuOut()
+    {
+        for(int i = 0; i < Menus.Length; i++)
+        {
+            Menus[i].transform.localPosition = new Vector3(5,0,0);
+        }
+    }
+}
