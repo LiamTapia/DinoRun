3e228fb5c175cbdc65cc5dfc19a80bbe22772fa5
Isaac Hernandez <isaac.hernandez.cano@uabc.edu.mx>
2021-05-16 20:01:05 -0700

    Update con logica de objetos muerte de jugador escena nivel del espacio crouching

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public void EndGame(){
+        if(gameHasEnded == false){
+            gameHasEnded = true;
+            Restart();
+        }
+    }
+
+    void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+    
---
+++ Assets/Scripts/SkullBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullBehaviour : Interactable
+{
+    private DinoCharacterController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = DinoCharacterController.FindObjectOfType<DinoCharacterController>();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+    public override void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Pickup(other));
+    }
+
+    IEnumerator Pickup(Collider other){
+        player.isInvinsible = true;
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(5);
+        player.isInvinsible = false;
+        Destroy(gameObject);
+    }
+}
