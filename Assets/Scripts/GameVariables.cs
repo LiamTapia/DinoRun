73aa54a964c700b8b105046b11fe29855fb3fbf5
LiamTapia <36176046+LiamTapia@users.noreply.github.com>
2021-05-18 12:37:54 -0700

    Cambio de escena y variables globales de juego

---
+++ Assets/Scripts/GameVariables.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariables 
+{
+    /*Valores del 1 al 3*/
+    public static int musicSelection;
+    public static int stageSelection;
+    public static int dinoSelection;
+    public static int difficultySelection;
+}
--- Assets/Scripts/MenuStartGame.cs
+++ Assets/Scripts/MenuStartGame.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuStartGame : MenuHorizontal
 {
     public override void Update()
     {
         base.Update();
+    }
+
+    public void difficultySelect(int selection)
+    {
+        GameVariables.difficultySelection = selection;
+    }
+
+    public void dinoSelect(int selection)
+    {
+        GameVariables.dinoSelection = selection;
+    }
+
+    public void musicSelect(int selection)
+    {
+        GameVariables.musicSelection = selection;
+    }
+
+    public void stageSelection(int selection)
+    {
+        GameVariables.stageSelection = selection;
+        SceneManager.LoadScene("SpaceLevel");
     }
 }
